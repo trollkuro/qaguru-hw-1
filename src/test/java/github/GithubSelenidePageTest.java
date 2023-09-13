26e0a3545e4f8b707da9cf58b3d7d97d1b232cbe
Kristina Egorova <kristi.egorova@gmail.com>
2023-09-13 17:16:23 +0300

    fixed homework for lesson #4, answer file

--- src/test/java/github/GithubSelenidePageTest.java
+++ src/test/java/github/GithubSelenidePageTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GithubSelenidePageTest {
@@ -16,7 +15,7 @@ public class GithubSelenidePageTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         open("https://github.com");
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -25,12 +24,37 @@ public class GithubSelenidePageTest {
         $("#query-builder-test").setValue("selenide").pressEnter();
         $("[data-testid=results-list]").$(".search-title").shouldHave(text("selenide/selenide")).click();
         $("#wiki-tab").click();
-        $(".markdown-body").$(byText("Soft assertions")).click();
+        $(".wiki-more-pages-link button").click();
+        $$("[data-filterable-for=wiki-pages-filter] li").findBy(text("SoftAssertions")).$("a").click();
         //check if JUnit5 is exist
         $$(".markdown-body h4").filterBy(text("JUnit5")).shouldHave(size(1));
         //check first code block of JUnit5
-        $$(".markdown-body pre").filterBy(text("@ExtendWith")).shouldHave(size(1));
+        $(".markdown-body").shouldHave(text(
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
         //check second code block of JUnit5
-        $$(".markdown-body pre").filterBy(text("@RegisterExtension")).shouldHave(size(1));
+        $(".markdown-body").shouldHave(text(
+                "class Tests {\n" +
+                "  @RegisterExtension\n" +
+                "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();" +
+                "\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
     }
 }
