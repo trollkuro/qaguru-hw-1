fd0b013a0384067349b5cdf35ac81763687b1b5d
Kristina Egorova <kristi.egorova@gmail.com>
2023-09-14 19:29:32 +0300

    task#2 for the lesson 5

---
+++ src/test/java/herokuapp/DrugAndDropPageTests.java
@@ -0,0 +1,28 @@
+package herokuapp;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DrugAndDropPageTests {
+    @BeforeAll
+    public static void browserSetUp() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void dragAndDropTest() {
+        $("#column-a").$("header").shouldHave(text("A"));
+        $("#column-b").$("header").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(to("#column-b"));
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+    }
+}
